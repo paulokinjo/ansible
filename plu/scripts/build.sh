addbd52d238f03d7a5d0f38df73246798ff18d33
Paulo Kinjo <paulokinjo@hotmail.com>
2019-01-19 14:23:33 +0000

    Add ssh automatically function

--- plu/scripts/build.sh
+++ plu/scripts/build.sh
@@ -4,6 +4,6 @@ echo "building images"
 echo "starting..."
 
 echo "ubuntu:ansible"
-docker build ./ubuntu/ -t ubuntu:ansible
+docker build ../ubuntu/ -t ubuntu:ansible
 echo "centos:ansible"
-docker build ./centos/ -t centos:ansible
+docker build ../centos/ -t centos:ansible
